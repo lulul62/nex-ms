104085568bdfca13081428ba57d8e5b46ddfde40
Leplan Ludwig <leplan.ludwig@gmail.com>
2018-11-08 16:02:50 +0100

    Add files via upload

--- jira.js
+++ jira.js
@@ -31,7 +31,7 @@ router.post('/rest/api/run', cache('5 minutes'), (req, res, next) => {
 router.post('/rest/api/runWithChangelog', cache('5 minutes'), (req, res, next) => {
     let options = {
         rejectUnauthorized: false,
-        url: 'https://jira.nexity.fr/rest/api/2/search?jql=project=' + req.body.applicationId + ' AND status not in ("12. Fermée", Abandonnée, "11. Livrée en Production")&maxResults=-1&expand=changelog&fields=""',
+        url: 'https://jira.nexity.fr/rest/api/2/search?jql=project=' + req.body.applicationId + ' AND status not in ("12. Fermée", Abandonnée, "11. Livrée en Production")&maxResults=-1&expand=changelog&fields="components"',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': req.body.token
